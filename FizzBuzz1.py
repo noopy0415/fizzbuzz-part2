976a1b91a320cb6c944ab7fa9c58676abb29fc08
noopy0415 <noopy0415@gmail.com>
2018-11-17 17:02:07 +0900

    1から100まで繰り返し
    range(start,stop[,step])

--- FizzBuzz1.py
+++ FizzBuzz1.py
@@ -1,14 +1,9 @@
 def fizzbuzz_convert(number):
-    if number % 15 == 0:
-        return "FizzBuzz"
-
-    if number % 3 == 0:
-        return "Fizz"
-
-    if number % 5 == 0: return "Buzz"
-
+    if number % 15 == 0:    return "FizzBuzz"
+    if number % 3 == 0:     return "Fizz"
+    if number % 5 == 0:     return "Buzz"
     return str(number)
 
 
-result = fizzbuzz_convert(5)
-print(result)
+for number in range(1, 101):
+    print(fizzbuzz_convert(number))
