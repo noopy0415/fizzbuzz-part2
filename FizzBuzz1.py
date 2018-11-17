beaf5926f9e44dfc31c350ce7589a850f074b33d
noopy0415 <noopy0415@gmail.com>
2018-11-17 16:35:07 +0900

    fizzbuzz変換用の関数を再デザイン
    出力して返り値がなかったので
    出力をやめて返り値とした
    
    ガード節を採用した

--- FizzBuzz1.py
+++ FizzBuzz1.py
@@ -1,23 +1,14 @@
 def fizzbuzz_convert(number):
     if number % 15 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(str(number))
+        return "FizzBuzz"
+
+    if number % 3 == 0:
+        return "Fizz"
+
+    if number % 5 == 0: return "Buzz"
+
+    return str(number)
 
 
-fizzbuzz_convert(1)
-fizzbuzz_convert(2)
-fizzbuzz_convert(3)
-fizzbuzz_convert(4)
-fizzbuzz_convert(5)
-fizzbuzz_convert(6)
-fizzbuzz_convert(10)
-fizzbuzz_convert(15)
-fizzbuzz_convert(97)
-fizzbuzz_convert(98)
-fizzbuzz_convert(99)
-fizzbuzz_convert(100)
+result = fizzbuzz_convert(5)
+print(result)
